e938c3786f6b43c620fee8fc2a6d3142956b037f
kaua-hiro <kauahiro2004@hotmail.com>
2024-10-15 17:11:28 -0300

    melhor?

--- DSM/teste3/script.js
+++ DSM/teste3/script.js
@@ -5,12 +5,12 @@ function mostrarImagem(n) {
     const imagens = document.querySelectorAll('.imagens img');
     const totalImagens = imagens.length;
 
-    // Se a imagem for maior que o número total, volta ao início
+
     if (n >= totalImagens) index = 0;
-    // Se a imagem for menor que 0, vai para a última
+
     if (n < 0) index = totalImagens - 1;
 
-    // Movimenta a galeria horizontalmente com base na imagem atual
+
     const translateX = -index * 100; 
     document.querySelector('.imagens').style.transform = `translateX(${translateX}%)`;
 }
